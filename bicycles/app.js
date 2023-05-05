5a0a589422adc0e6dfc59067595a60d1658e1046
mherreral <mherlop77@gmail.com>
2023-05-04 22:41:48 -0500

    updated prj

--- bicycles/app.js
+++ bicycles/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var bicyclesAPIRouter = require("./routes/api/bicycles");
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use("/", bicyclesAPIRouter);
 
