3bedd16939727dc226ebee1376191ef7826ca0cb
mherreral <mherlop77@gmail.com>
2023-05-04 22:40:53 -0500

    added map localization

--- frontend/public/javascripts/map.js
+++ frontend/public/javascripts/map.js
@@ -1,10 +1,20 @@
-var map = L.map('map').setView([6.2476, -75.5658], 13);
+var map = L.map('map').setView([39.757361, -105.109869], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([6.2476, -75.5658]).addTo(map)
-    .bindPopup('My bici')
-    .openPopup();
+$.ajax({
+    datatype: "json",
+    url: "http://127.0.0.1:5000/api/v1/list",
+    success: function (results) {
+        console.log(results);
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i]._id);
+            L.marker(results[i].location, {
+                title: "Bicicleta Nº: " + results[i]._id + " Modelo: " + results[i].model
+            }).addTo(map).bindPopup("Bicicleta Nº: " + results[i]._id + " Modelo: " + results[i].model)
+        }
+    }
+});
